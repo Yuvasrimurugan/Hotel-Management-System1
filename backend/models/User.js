8cee0d0b77c6a207ad0030f787104bfe11c883de
arunkumar <arunkumar_19@outlook.com>
2026-07-30 14:54:37 +0530

    Updated User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const db = require("../config/db");
 
 const createUser = async (full_name, email, password, role) => {
@@ -27,11 +26,9 @@ const findUserByEmail = async (email) => {
 module.exports = {
   createUser,
   findUserByEmail,
-=======
 exports.createUser = async (user) => {
     return db.query(
         "INSERT INTO users(name,email,password) VALUES(?,?,?)",
         [user.name, user.email, user.password]
     );
->>>>>>> a0661921be0c44435a670e2ef7f12de7f0cc07ad
 };
